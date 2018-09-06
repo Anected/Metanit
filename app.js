c4bebf28fd472f6eeeb27281890529bced381988
Alexander <anected@gmail.com>
2018-09-06 22:28:25 +0300

    'mark1'

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ var app = express();
 var jsonParser = bodyParser.json();
 
 app.use(express.static(__dirname + "/public"));
-// получение списка данных
-app.get("/api/users", function(req, res){
-
-    var content = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(content);
-    res.send(users);
-});
 
 app.listen(3000, function(){
     console.log("Сервер ожидает подключения...");
